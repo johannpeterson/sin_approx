bf469d51ec32385ff4d9b11504f9f65d5ef8147a
Johann Peterson <johann.peterson@gmail.com>
2024-10-30 20:25:14 -0700

    combined library libmysin.dylib

--- sin/benchmark.c
+++ sin/benchmark.c
@@ -1,39 +1,65 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 #include <gsl/gsl_math.h>
-#include <gsl/gsl_sf_trig.h>
-#include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_sf_trig.h>
 #include <gsl/gsl_statistics_double.h>
 
-extern double sin_3(double x);
+#include "mysin.h"
 
-gsl_rng * r;  /* global generator */
+char* help_string =
+  "benchmark - Timing program for sin functions.\n"
+  "usage: benchmark [-p npoints] [-c ncycles] [-m min -M max]\n\n"
+  "    -p npoints          The number of x values to use for each cycle of testing\n"
+  "                        Default 10000\n"
+  "    -c ncycles          The number of test cycles to perform.\n"
+  "                        Default 3\n"
+  "    -m min -M max       Specify a range from which to draw x values for testing Sin(x).\n"
+  "                        Defaults min=-Pi max=Pi\n";
+
+gsl_rng *r; /* global random number generator */
 
 int main(int argc, char **argv) {
   int c;
-  int points = 100;
+  int points = 10000;
   int cycles = 3;
-  while ((c = getopt(argc, argv, "p:")) != -1) {
+  double min = -M_PI;
+  double max = M_PI;
+  while ((c = getopt(argc, argv, "c:p:m:M:h")) != -1) {
     switch (c) {
     case 'p':
       points = atoi(optarg);
       break;
+    case 'c':
+      cycles = atoi(optarg);
+      break;
+    case 'm':
+      min = atof(optarg);
+      break;
+    case 'M':
+      max = atof(optarg);
+      break;
+    case 'h':
+      printf("%s\n", help_string);
+      exit(0);
     }
   }
 
+  // check parameters
   if (points < 1) {
-    printf("Please enter a positive number of points to test.");
+    printf("Please specify a positive number of test points per cycle.");
     exit(1);
   }
   if (cycles < 1) {
-    printf("Please enter a positive number of cycles.");
+    printf("Please specify a positive number of test cycles.");
     exit(1);
   }
 
+  // allocate memory
   double *x = malloc(points * sizeof(double));
   double *y1 = malloc(points * sizeof(double));
   double *y2 = malloc(points * sizeof(double));
@@ -44,13 +70,13 @@ int main(int argc, char **argv) {
   }
 
   // set up RNG
-  const gsl_rng_type * T;
+  const gsl_rng_type *T;
   gsl_rng_env_setup();
   T = gsl_rng_default;
-  r = gsl_rng_alloc (T);
+  r = gsl_rng_alloc(T);
 
-  printf ("generator type: %s\n", gsl_rng_name (r));
-  printf ("seed = %lu\n", gsl_rng_default_seed);
+  printf("generator type: %s\n", gsl_rng_name(r));
+  printf("seed = %lu\n", gsl_rng_default_seed);
 
   clock_t begin, end;
   double time_spent;
@@ -58,7 +84,7 @@ int main(int argc, char **argv) {
   for (int c = 1; c <= cycles; c++) {
     printf("cycle: %i\n", c);
     for (int i = 0; i < points; i++) {
-      x[i] = gsl_ran_flat(r, 0, M_PI);
+      x[i] = gsl_ran_flat(r, 10e9 - M_PI, 10e9 + M_PI);
     }
 
     // timing
@@ -82,14 +108,13 @@ int main(int argc, char **argv) {
       err[i] = y1[i] - y2[i];
     }
     double min, max;
-    printf("mean(err) = %.17f\n", gsl_stats_mean(err, 1, points));
+    printf("mean(err) = %e\n", gsl_stats_mean(err, 1, points));
     gsl_stats_minmax(&min, &max, err, 1, points);
-    printf("range(err) = %.17f - %.17f\n", min, max);
-
-    }
+    printf("range(err) = %e - %e\n", min, max);
+  }
 
   // clean up
-  gsl_rng_free (r);
+  gsl_rng_free(r);
   free(x);
   free(y1);
   free(y2);
---
+++ sin/mysin.h
@@ -0,0 +1,3 @@
+extern double sin_1(double x);
+extern double sin_2(double x);
+extern double sin_3(double x);
